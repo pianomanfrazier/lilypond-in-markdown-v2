27851f82ae112e3557784f20a62f649fc1d11d6a
Ryan Frazier <pianomanfrazier@gmail.com>
2019-09-02 09:48:33 -0600

    Cleanup dead code
    - lilypond is now supported in Prism so can remove my own syntax

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,10 @@
 /* eslint-disable max-len */
 const CleanCSS = require('clean-css');
 const UglifyJS = require('uglify-es');
-const htmlmin = require('html-minifier');
 const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const md5 = require('crypto-js/md5');
-const fs = require('fs');
-const {exec} = require('child_process');
-const {svgo} = require('./svgoConfig');
 const {lilypondExtension} = require('./lilypondExtension');
 const {lilycodeExtension} = require('./lilycodeExtension');
 const {lilyaudioExtension} = require('./lilyaudioExtension');
-const {lilypondSyntax} = require('./lilypondSyntax.js')
 
 // Setup up typography
 const Typography = require('typography');
@@ -19,21 +13,8 @@ const Typography = require('typography');
 const theme = require('typography-theme-funston');
 const typography = new Typography(theme);
 
-// used to minify the svg output of Lilypond
-const minifyHtml = (content) => htmlmin.minify(content, {
-  useShortDoctype: true,
-  removeComments: true,
-  collapseWhitespace: true,
-});
-
 module.exports = function(eleventyConfig) {
-
-   eleventyConfig.addPlugin(pluginSyntaxHighlight, {
-     init: function({Prism}) {
-       Prism.languages.lilypond = lilypondSyntax;
-       Prism.languages.ly = Prism.languages.lilypond;
-     },
-   });
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
 
   eleventyConfig.addFilter('cssmin', function(code) {
     return new CleanCSS({}).minify(code).styles;
